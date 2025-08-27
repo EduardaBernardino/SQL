90e8cdc13ae2ff20629e7865e474ac93374ed082
Eduarda Bernardino <eduardabernardinoo06@gmail.com>
2025-08-27 20:19:25 -0300

    Create aula.sql

---
+++ aula.sql
@@ -0,0 +1,14 @@
+AUTO_INCREMENT
+--Elemento Auto_Increment: toda vez que for acrescido um registro ele vai se autoincrementar
+
+--Codigo executável: é um código em binário "exe"
+
+--Todo e qualquer processo para chegar no processador precisa antes ter sido carregado na memória principal. O sistema operacional gerencia memoria, entrada e saída, etc
+
+
+--Variável estática: é como se ela tivesse uma gaveta só para ela (registro base), então se eu quiser muda-la eu vou nessa gaveta. 
+--Variável estática: pode estar em qualquer espaço que não tem nada alocado. Mas ela tem que ter uma âncora. Ex: nome
+
+--Estudar árvore B
+
+
